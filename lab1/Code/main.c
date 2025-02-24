898da81f11ca0afd4a13b6dd1bbe37d079736329
gryz <jinlizhong@smail.nju.edu.cn>
2025-02-24 03:30:46 +0000

    添加了有关词法单元位置信息的支持

--- lab1/Code/main.c
+++ lab1/Code/main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-extern FILE* yyin;
 extern int yyparse();
+extern int yyrestart(FILE *);
+
+void yyerror(const char *s) {
+    fprintf(stderr, "Syntax error: %s\n", s);
+}
 
 int main(int argc, char const *argv[]) {
+    FILE *f = NULL;
     if (argc > 1) {
-        if (!(yyin = fopen(argv[1], "r"))) {
+        if (!(f = fopen(argv[1], "r"))) {
             perror(argv[1]);
             return 1;
         }
+        yyrestart(f);
     }
     yyparse();
     return 0;
--- lab1/Code/syntax.y
+++ lab1/Code/syntax.y
@@ -1,10 +1,12 @@
+%locations
 %{
     #include "lex.yy.c"
+    int yyerror(const char *s);
 %}
 
 %token INT
 %token WORD
 
 %%
-Program : INT WORD
+Program : INT WORD { printf("haha: %d, hehe %d\n", $1, $2); }
 %%
