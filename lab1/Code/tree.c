393c388d960db745fee2806f50a820cb88a4a4c5
gryz <jinlizhong@smail.nju.edu.cn>
2025-02-25 02:07:51 +0000

    分开了树新建节点与连接父子的API

--- lab1/Code/tree.c
+++ lab1/Code/tree.c
@@ -2,16 +2,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-tree_node_t *tree_add_child(tree_node_t *father) {
+void tree_add_child(tree_node_t *father, tree_node_t *child) {
+    child->next_brother = father->first_child;
+    father->first_child = child;
+}
+
+tree_node_t *tree_new_node() {
     tree_node_t *new_node = malloc(sizeof(tree_node_t));
-    if (!new_node) return NULL;
     new_node->first_child = NULL;
-    new_node->next_brother = father->first_child;
-    father->first_child = new_node;
+    new_node->next_brother = NULL;
     return new_node;
 }
 
 void tree_init_root() {
-    tree_root = malloc(sizeof(tree_node_t));
-    tree_root->first_child = NULL;
+    tree_root = tree_new_node();
 }
--- lab1/Code/tree.h
+++ lab1/Code/tree.h
@@ -9,5 +9,6 @@ struct tree_node_t {
 typedef struct tree_node_t tree_node_t;
 
 tree_node_t* tree_root;
-tree_node_t* tree_add_child(tree_node_t *father);
+void tree_add_child(tree_node_t *father, tree_node_t *child);
+tree_node_t* tree_new_node();
 void tree_init_root();
