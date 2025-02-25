25f23fe9a7bd1d311c854588e5d4aa3bb0cfee42
gryz <jinlizhong@smail.nju.edu.cn>
2025-02-25 11:45:02 +0000

    树API不再分配节点内存, 由调用者自行分配

--- lab1/Code/tree.h
+++ lab1/Code/tree.h
@@ -11,9 +11,7 @@ struct tree_node_t {
 };
 typedef struct tree_node_t tree_node_t;
 
-tree_node_t* tree_root;
 void tree_add_child(tree_node_t *father, tree_node_t *child);
-tree_node_t* tree_new_node();
-void tree_init_root();
+void tree_init_node(tree_node_t *node);
 
 #endif
