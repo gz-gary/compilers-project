eda766247fc5ad7643a9834528378903ae0d7cf1
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-03 08:02:22 +0000

    添加了生成提交压缩包的脚本

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,7 @@ RUN apt-get update && apt-get install -y -qq \
     gdb \
     git \
     vim \
+    zip \
     python3.8 \
     python3-pip \
     python2-minimal && \
---
+++ lab1/submit_gen.sh
@@ -0,0 +1,14 @@
+rm -rf submit
+mkdir -p submit/Code
+cp Code/*.c submit/Code
+cp Code/*.h submit/Code
+cp Code/*.l submit/Code
+cp Code/*.y submit/Code
+cp Code/Makefile submit/Code
+cp doc/report.pdf submit
+cp README submit
+cd Code && make
+cp parser ../submit
+make clean
+cd ..
+zip -r submit.zip submit
