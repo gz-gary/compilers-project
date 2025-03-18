5c760a63359a6b0e9ce9bc651bd42d4e950b690d
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-18 07:33:46 +0000

    实现拉链式哈希表

---
+++ lab2/Code/hashtable.h
@@ -0,0 +1,15 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+struct hashtable_entry_t {
+    const char *key;
+    struct hashtable_entry_t *next;
+};
+typedef struct hashtable_entry_t hashtable_entry_t;
+
+void hashtable_init_entry(hashtable_entry_t *entry, const char *key);
+unsigned int hashtable_add_entry(hashtable_entry_t *entry);
+void hashtable_remove_entry(unsigned int slot_idx);
+hashtable_entry_t* hashtable_query_entry(const char *key);
+
+#endif
