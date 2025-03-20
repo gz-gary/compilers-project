d6128248a2a697f2292eb4d4eac02bc9b0cbaabe
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-20 02:33:35 +0000

    符号表实现为符号到类型的映射

--- lab2/Code/symbtable.c
+++ lab2/Code/symbtable.c
@@ -1,10 +1,15 @@
 #include "symbtable.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-void symbtable_add_entry(const char *symb) {
+void symbtable_add_entry(const char *symb, type_t *type) {
     symbtable_entry_t *entry = malloc(sizeof(symbtable_entry_t));
 
     entry->symb = symb;
+    entry->type = type;
+    printf("%s -> ", symb);
+    log_type(type);
+    printf("\n");
 
     hashtable_init_entry(&(entry->hashtable_entry), symb);
     hashtable_add_entry(&(entry->hashtable_entry));
--- lab2/Code/symbtable.h
+++ lab2/Code/symbtable.h
@@ -1,16 +1,18 @@
 #ifndef SYMBTABLE_H
 #define SYMBTABLE_H
 
+#include "type.h"
 #include "hashtable.h"
 #define hash2symb(entry) (symbtable_entry_t *)((void *)(entry) - (void *)(&(((symbtable_entry_t *)(0))->hashtable_entry)))
 
 struct symbtable_entry_t {
     const char *symb;
+    type_t *type;
     hashtable_entry_t hashtable_entry;
 };
 typedef struct symbtable_entry_t symbtable_entry_t;
 
-void symbtable_add_entry(const char *symb);
+void symbtable_add_entry(const char *symb, type_t *type);
 symbtable_entry_t* symbtable_query_entry(const char *symb);
 
 #endif
