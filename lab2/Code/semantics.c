54af6c3262df8745434a5edc9585f1ac987ec75d
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-21 07:11:26 +0000

    实现了初步语义检查: 符号-类型关联表&重定义符号检查

---
+++ lab2/Code/semantics.c
@@ -0,0 +1,251 @@
+#include "semantics.h"
+#include "symbtable.h"
+#include "production.h"
+#include "log.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+type_t* handle_specifier(ast_node_t *specifier);
+
+void handle_exp(ast_node_t *ast_exp) {
+    /*
+        检查最底层的标识符产生式
+        1. Exp -> ID
+        2. Exp -> ID LP RP
+        3. Exp -> ID LP Args RP
+    */
+    /*{
+        production_rec_t recs[1] = {
+            {NULL, AST_NODE_ID}
+        };
+        if (production_match(ast_exp, recs, 1)) {
+            const char *symb = recs[0].ast_node->attr.identifier_value;
+            if (!symbtable_query_entry(symb)) {
+                log_semantics_error_prologue("1", recs[0].ast_node->lineno);
+                fprintf(stdout, "Undefined variable \"%s\".\n", symb);
+            }
+        }
+    }
+    {
+        production_rec_t recs[3] = {
+            {NULL, AST_NODE_ID},
+            {NULL, AST_NODE_LP},
+            {NULL, AST_NODE_RP}
+        };
+        if (production_match(ast_exp, recs, 3)) {
+            const char *symb = recs[0].ast_node->attr.identifier_value;
+            if (!symbtable_query_entry(symb)) {
+                log_semantics_error_prologue("2", recs[0].ast_node->lineno);
+                fprintf(stdout, "Undefined function \"%s\".\n", symb);
+            }
+        }
+    }
+    {
+        production_rec_t recs[4] = {
+            {NULL, AST_NODE_ID},
+            {NULL, AST_NODE_LP},
+            {NULL, AST_NODE_Args},
+            {NULL, AST_NODE_RP}
+        };
+        if (production_match(ast_exp, recs, 4)) {
+            const char *symb = recs[0].ast_node->attr.identifier_value;
+            if (!symbtable_query_entry(symb)) {
+                log_semantics_error_prologue("2", recs[0].ast_node->lineno);
+                fprintf(stdout, "Undefined function \"%s\".\n", symb);
+            }
+        }
+    }*/
+}
+
+type_t *handle_vardec(type_t *specifier_type, ast_node_t *vardec, const char **name) {
+    ast_node_t *first_child = ast_1st_child(vardec);
+    if (first_child->node_type == AST_NODE_ID) {
+        *name = first_child->attr.identifier_value;
+        return specifier_type;
+    } else return handle_vardec(type_new_array(specifier_type), first_child, name);
+}
+
+void handle_deflist(ast_node_t *deflist, type_t *upper_struct) {
+    while (!production_epsilon(deflist)) {
+        ast_node_t *def = ast_1st_child(deflist);
+        ast_node_t *rest = ast_2nd_child(deflist);
+
+        ast_node_t *def_spec = ast_1st_child(def);
+        ast_node_t *declist = ast_2nd_child(def);
+        type_t *spec_type = handle_specifier(def_spec);
+        while (1) {
+            ast_node_t *dec = ast_1st_child(declist);
+            ast_node_t *vardec = ast_1st_child(dec);
+
+            const char *name;
+            type_t *type = handle_vardec(spec_type, vardec, &name);
+            if (symbtable_query_entry(name)) {
+                log_semantics_error_prologue("15", vardec->lineno);
+                fprintf(stdout, "Redefined %s \"%s\".\n", upper_struct == NULL ? "variable" : "field", name);
+            } else symbtable_add_entry(name, upper_struct == NULL ? SYMB_VAR : SYMB_FIELD, type);
+            if (upper_struct)
+                type_add_struct_field(upper_struct, type, name);
+            
+            if (ast_onlyone_child(declist)) break;
+            ast_node_t *rest = ast_3rd_child(declist);
+            declist = rest;
+        }
+
+        deflist = rest;
+    }
+}
+
+type_t* handle_specifier(ast_node_t *specifier) {
+    ast_node_t *first_child = ast_1st_child(specifier);
+    if (first_child->node_type == AST_NODE_TYPE) {
+        /* case 1: basic type specifier */
+        const char *typename = first_child->attr.typename_value;
+        if (!strcmp(typename, "int")) return type_new_basic_int();
+        if (!strcmp(typename, "float")) return type_new_basic_float();
+
+    } else if (first_child->node_type == AST_NODE_StructSpecifier) {
+        {
+            /* case 2: using defined struct specifier */
+            production_rec_t recs[2] = {
+                {NULL, AST_NODE_STRUCT},
+                {NULL, AST_NODE_Tag}
+            };
+            if (production_match(first_child, recs, 2)) {
+                const char *symb = tree2ast((recs[1].ast_node)->tree_node.first_child)->attr.identifier_value;
+                symbtable_entry_t *entry = symbtable_query_entry(symb);
+                if (!entry || entry->symb_type != SYMB_STRUCT) {
+                    log_semantics_error_prologue("17", recs[0].ast_node->lineno);
+                    fprintf(stdout, "Undefined structure \"%s\".\n", symb);
+                    return type_new_invalid();
+                } else return entry->type;
+            }
+        }
+        {
+            /* case 3: define new struct specifier */
+            production_rec_t recs[5] = {
+                {NULL, AST_NODE_STRUCT},
+                {NULL, AST_NODE_OptTag},
+                {NULL, AST_NODE_LC},
+                {NULL, AST_NODE_DefList},
+                {NULL, AST_NODE_RC},
+            };
+            if (production_match(first_child, recs, 5)) {
+                const char *symb = production_epsilon(recs[1].ast_node) ?
+                    NULL :
+                    ast_1st_child(recs[1].ast_node)->attr.identifier_value;
+
+                ast_node_t *deflist = recs[3].ast_node;
+                type_t *result = type_new_struct();
+                handle_deflist(deflist, result);
+                if (symb) {
+                    symbtable_entry_t *entry = symbtable_query_entry(symb);
+                    if (entry) {
+                        log_semantics_error_prologue("16", (recs[1].ast_node)->lineno);
+                        fprintf(stdout, "Redefined structure \"%s\".\n", symb);
+                    } else symbtable_add_entry(symb, SYMB_STRUCT, result);
+                }
+
+                return result;
+            }
+        }
+
+    }
+}
+
+void handle_compst(ast_node_t *compst) {
+    ast_node_t *deflist = ast_2nd_child(compst);
+    handle_deflist(deflist, NULL);
+}
+
+void handle_extdef(ast_node_t *extdef) {
+    ast_node_t *specifier = ast_1st_child(extdef);
+    type_t *spec_type = handle_specifier(specifier);
+    ast_node_t *tbd = ast_2nd_child(extdef);
+    switch (tbd->node_type) {
+    case AST_NODE_ExtDecList:
+        {
+            ast_node_t *extdeclist = tbd;
+            while (1) {
+                ast_node_t *vardec = ast_1st_child(extdeclist);
+                const char *name;
+                type_t *type = handle_vardec(spec_type, vardec, &name);
+
+                if (symbtable_query_entry(name)) {
+                    log_semantics_error_prologue("3", vardec->lineno);
+                    fprintf(stdout, "Redefined variable \"%s\".\n", name);
+                } else symbtable_add_entry(name, SYMB_VAR, type);
+
+                if (ast_onlyone_child(extdeclist)) break;
+                extdeclist = ast_3rd_child(extdeclist);
+            }
+        }
+        break;
+    case AST_NODE_FunDec:
+        {
+            ast_node_t *fundec = tbd;
+            ast_node_t *id = ast_1st_child(fundec);
+            ast_node_t *tbd_ = ast_3rd_child(fundec);
+            const char *name = id->attr.identifier_value;
+            type_t *func_type = type_new_func(spec_type);
+            if (tbd_->node_type == AST_NODE_VarList) {
+                ast_node_t *varlist = tbd_;
+                while (1) {
+                    ast_node_t *paramdec = ast_1st_child(varlist);
+
+                    ast_node_t *param_spec = ast_1st_child(paramdec);
+                    ast_node_t *vardec = ast_2nd_child(paramdec);
+                    const char *param_name;
+                    type_t *param_spec_type = handle_specifier(param_spec);
+                    type_t *param_type = handle_vardec(param_spec_type, vardec, &param_name);
+
+                    if (symbtable_query_entry(param_name)) {
+                        log_semantics_error_prologue("3", vardec->lineno);
+                        fprintf(stdout, "Redefined variable \"%s\".\n", param_name);
+                    } else {
+                        symbtable_add_entry(param_name, SYMB_VAR, param_type);
+                        type_add_func_arg(func_type, param_type);
+                    }
+                    
+                    if (ast_onlyone_child(varlist)) break;
+                    ast_node_t *rest = ast_3rd_child(varlist);
+                    varlist = rest;
+                }
+            }
+            if (symbtable_query_entry(name)) {
+                log_semantics_error_prologue("3", id->lineno);
+                fprintf(stdout, "Redefined function \"%s\".\n", name);
+            } else symbtable_add_entry(name, SYMB_FUNC, func_type);
+
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void handle_node(ast_node_t *ast_node, int depth) {
+    switch (ast_node->node_type) {
+    /* case AST_NODE_Def: */
+    /* 不直接处理Def而是在Toplevel处理: CompSt/ExtDef */
+    case AST_NODE_CompSt:
+        handle_compst(ast_node);
+        break;
+    //case AST_NODE_Exp:
+        //handle_exp(ast_node);
+        //break;
+    case AST_NODE_ExtDef:
+        handle_extdef(ast_node);
+        break;
+    default:
+        break;
+    }
+}
+
+void semantics_check() {
+    /* definition checking */
+    ast_walk(handle_node, ast_walk_action_nop);
+    /* type checking */
+    // ast_walk(..., ...);
+}
---
+++ lab2/Code/semantics.h
@@ -0,0 +1,3 @@
+#include "ast.h"
+
+void semantics_check();
