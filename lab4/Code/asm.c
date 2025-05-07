d20a8dc1baaccf10de3ef0db4f3a7f96d9201f81
gryz <jinlizhong@smail.nju.edu.cn>
2025-05-07 13:59:53 +0000

    添加了预置的read和write函数

--- lab4/Code/asm.c
+++ lab4/Code/asm.c
@@ -605,6 +605,27 @@ void asm_set_output_file(FILE *f) {
 }
 
 void asm_dump() {
+    fprintf(output_file, ".data\n\
+_prompt: .asciiz \"Enter an integer:\"\n\
+_ret: .asciiz \"\\n\"\n\
+.globl main\n\
+.text\n\
+read:\n\
+li $v0, 4\n\
+la $a0, _prompt\n\
+syscall\n\
+li $v0, 5\n\
+syscall\n\
+jr $ra\n\
+\n\
+write:\n\
+li $v0, 1\n\
+syscall\n\
+li $v0, 4\n\
+la $a0, _ret\n\
+syscall\n\
+move $v0, $0\n\
+jr $ra\n\n");
     asm_code_t *code = head.next;
     while (code != NULL) {
         switch (code->instr) {
