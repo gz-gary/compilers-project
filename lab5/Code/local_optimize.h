1d9de235153885e5e60268ac897a6d1cc44305a3
Jaderest <jaderest233@outlook.com>
2025-06-01 03:00:59 +0800

    填完常量分析的空了

--- lab5/Code/local_optimize.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOCAL_OPTIMIZE_H__
-#define __LOCAL_OPTIMIZE_H__
-
-#include "IR_parse.h"
-
-void local_optimize();
-
-#endif // __LOCAL_OPTIMIZE_H__
--- lab5/Code/main.c
+++ lab5/Code/main.c
@@ -1,13 +1,16 @@
 #include <time.h>
 #include <stdio.h>
 #include "IR_parse.h"
-#include "local_optimize.h"
+#include "IR_optimize.h"
+// #include "local_optimize.h"
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     IR_parse(argc >= 2 ? argv[1] : NULL);
     // TODO: optimize
-    local_optimize();
+    // local_optimize();
+    IR_optimize();
+    
     IR_output(argc >= 3 ? argv[2] : NULL);
     return 0;
 }
